318cba53e448a1aaa0adc2d69927ef7ef555a35b
MFAman <fayzullahaman@gmail.com>
2022-09-22 11:59:15 +0600

    add project

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,8 +1,8 @@
 <?php
     define("HOST", "localhost");
-    define("USER", "mfaman");
+    define("USER", "u318076185_mfaman");
     define("PASS", "0YRi$]Fj:");
-    define("DB", "library");
+    define("DB", "u318076185_library");
 
     $db = new mysqli(HOST, USER, PASS, DB);
 ?>
