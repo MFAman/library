f1a813f8c926c9a975123187ece2f6c786ea8d3f
MFAman <fayzullahaman@gmail.com>
2022-09-22 11:48:35 +0600

    add library

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,7 +1,7 @@
 <?php
     define("HOST", "localhost");
-    define("USER", "root");
-    define("PASS", "");
+    define("USER", "mfaman");
+    define("PASS", "0YRi$]Fj:");
     define("DB", "library");
 
     $db = new mysqli(HOST, USER, PASS, DB);
